da37798b18a69bea84d854266974b18503838e2e
Leonardo Martins <lvmartins@bi4all.pt>
2024-08-09 12:55:33 +0000

    First models

---
+++ definitions/raw_declaration.js
@@ -0,0 +1,36 @@
+declare({
+  database: "act-acc-boms-prd",
+  schema: "act_acc_boms_raw_2",
+  name: "raw_ConfirmacaoProducao",
+});
+
+
+
+declare({
+  database: "act-acc-boms-prd",
+  schema: "act_acc_boms_raw_2",
+  name: "raw_OrdensProducao",
+});
+
+
+
+declare({
+  database: "act-acc-boms-prd",
+  schema: "act_acc_boms_data_csv_2",
+  name: "acc_master_data_2",
+});
+
+
+declare({
+  database: "act-acc-boms-prd",
+  schema: "act_acc_boms_data_csv_2",
+  name: "acc_ordens_producao",
+});
+
+
+
+declare({
+  database: "act-acc-boms-prd",
+  schema: "act_acc_boms_data_csv_2",
+  name: "acc_confirmacoes",
+});
---
+++ definitions/stg/stg_declaration.js
@@ -0,0 +1,17 @@
+declare({
+  database: "act-acc-boms-prd",
+  schema: "act_acc_boms_stg_2",
+  name: "stg_master_data",
+});
+
+declare({
+  database: "act-acc-boms-prd",
+  schema: "act_acc_boms_stg_2",
+  name: "stg_ordens_producao",
+});
+
+declare({
+  database: "act-acc-boms-prd",
+  schema: "act_acc_boms_stg_2",
+  name: "stg_confirmacoes_producao",
+});
